4ec47783d211d4e76f0a96d891e0a4a17f8bac0f
Pedro López Mareque <Pedro.lopez.mareque@gmail.com>
2025-06-07 11:04:04 +0200

    Revert "feat: solve day one 2024"
    This reverts commit 8fda4ce61033579aa9ac01a1d8c00c8653bbe2ec.

---
+++ src/dummy_class.py
@@ -0,0 +1,3 @@
+class Dummy:
+    def add(self, first: int, second: int) -> int:
+        return first + second
---
+++ tests/test_dummy_class.py
@@ -0,0 +1,12 @@
+from expects import equal, expect
+
+from src.dummy_class import Dummy
+
+
+class TestDummy:
+    def test_dummy(self) -> None:
+        dummy = Dummy()
+
+        result = dummy.add(1, 2)
+
+        expect(result).to(equal(3))
